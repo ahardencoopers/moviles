164df9f5966ebad0e25d223690a9654850e212c9
ahardencoopers <ahardencoopers@gmail.com>
2016-10-23 14:00:01 -0500

    Agregar codigo MainActivity.java

--- PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/MainActivity.java
+++ PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/MainActivity.java
@@ -1,13 +1,32 @@
 package itesm.mx.proyecto_moviles;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnComenzar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
+
+        btnComenzar = (Button) findViewById(R.id.comenzar);
+        btnComenzar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.comenzar:
+                Intent myIntent = new Intent(this, RegistrateActivity.class);
+                startActivity(myIntent);
+                break;
+        }
     }
 }
---
+++ PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/RegistrateActivity.java
@@ -0,0 +1,13 @@
+package itesm.mx.proyecto_moviles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrate);
+    }
+}
