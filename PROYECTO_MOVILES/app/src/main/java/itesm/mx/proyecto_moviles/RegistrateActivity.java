30f37719bcbb5e97b73ce2bd99e790c9442201d2
ahardencoopers <ahardencoopers@gmail.com>
2016-10-23 18:43:24 -0500

    Completar layout activity_registrate.xml

--- PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/RegistrateActivity.java
+++ PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/RegistrateActivity.java
@@ -2,12 +2,22 @@ package itesm.mx.proyecto_moviles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RegistrateActivity extends AppCompatActivity {
+
+    private Spinner spinnerSexo = null;
+    private String sSexos[] = {"Hombre", "Mujer"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrate);
+
+        spinnerSexo = (Spinner) findViewById(R.id.spinner_sexo);
+        ArrayAdapter<String> adapterSpinnerSexo = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sSexos);
+
+        spinnerSexo.setAdapter(adapterSpinnerSexo);
     }
 }
