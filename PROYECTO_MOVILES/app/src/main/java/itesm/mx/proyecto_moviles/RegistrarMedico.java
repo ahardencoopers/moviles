9cde3dcc158f967e054a7a5197e9c2737ba30054
ahardencoopers <ahardencoopers@gmail.com>
2016-10-23 20:38:20 -0500

    Agregar layout y activity para registrar medico

--- PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/RegistrarMedico.java
+++ PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/RegistrarMedico.java
@@ -2,12 +2,30 @@ package itesm.mx.proyecto_moviles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
-public class RegistrarMedico extends AppCompatActivity {
+public class RegistrarMedico extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnRegistrarMedico = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_medico);
+
+        btnRegistrarMedico = (Button) findViewById(R.id.boton_registrar_medico);
+
+        btnRegistrarMedico.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.boton_registrar_medico:
+                Intent myIntent = new Intent(this, MedicosRegistrados.class);
+                startActivity(myIntent);
+        }
     }
 }
