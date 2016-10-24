5ceb0f887f76a6ccb5e3ba935ef65703334b6b19
ahardencoopers <ahardencoopers@gmail.com>
2016-10-23 19:34:11 -0500

    Agregar onClick listener a boton agregar medicamentos de layout activity_agregar_medicamentos.xml

--- PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/AgregarMedicamentos.java
+++ PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/AgregarMedicamentos.java
@@ -1,12 +1,17 @@
 package itesm.mx.proyecto_moviles;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
-public class AgregarMedicamentos extends ListActivity {
+public class AgregarMedicamentos extends ListActivity implements View.OnClickListener {
+
+    private Button btnAgregarMedicamento = null;
 
     public ArrayList<Medicamento> getDataForListView() {
         Medicamento medicamento;
@@ -26,10 +31,23 @@ public class AgregarMedicamentos extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_medicamentos);
 
+        btnAgregarMedicamento = (Button) findViewById(R.id.button_agregar_medicamento);
+
         ArrayList <Medicamento> arrayListMedicamento;
         arrayListMedicamento = getDataForListView();
 
         MedicamentoAdapter adapterMedicamentos = new MedicamentoAdapter(this, arrayListMedicamento);
         setListAdapter(adapterMedicamentos);
+        btnAgregarMedicamento.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button_agregar_medicamento:
+                Intent myIntent = new Intent(this, AgregarMedicamento.class);
+                startActivity(myIntent);
+                break;
+        }
     }
 }
