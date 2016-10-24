f0ceef2f1b594ac4322bdf2d6020b4cdfda0c04c
ahardencoopers <ahardencoopers@gmail.com>
2016-10-24 11:32:57 -0500

    Broke multiple row layout

--- PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/CalendarioActivity.java
+++ PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/CalendarioActivity.java
@@ -2,12 +2,32 @@ package itesm.mx.proyecto_moviles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
-public class CalendarioActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class CalendarioActivity extends ListActivity {
+
+    private CalendarioAdapter calendarioAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendario);
+        calendarioAdapter = new CalendarioAdapter(this);
+        for (int i = 1; i < 50; i++) {
+            calendarioAdapter.addItem("item " + i);
+            if (i % 4 == 0) {
+                calendarioAdapter.addSeparatorItem("separator " + i);
+            }
+        }
+        setListAdapter(calendarioAdapter);
     }
 }
---
+++ PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/CalendarioAdapter.java
@@ -0,0 +1,109 @@
+package itesm.mx.proyecto_moviles;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+/**
+ * Created by achs on 24/10/16.
+ */
+class CalendarioAdapter extends BaseAdapter {
+
+    private static final int TYPE_ITEM = 0;
+    private static final int TYPE_SEPARATOR = 1;
+    private static final int TYPE_MAX_COUNT = TYPE_SEPARATOR + 1;
+
+    private ArrayList<String> mData = new ArrayList<String>();
+    private LayoutInflater mInflater;
+
+    private TreeSet<Integer> mSeparatorsSet = new TreeSet<Integer>();
+    private Context context;
+
+    public CalendarioAdapter(Context context) {
+        this.context = context;
+        mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void addItem(final String item) {
+        mData.add(item);
+        notifyDataSetChanged();
+    }
+
+    public void addSeparatorItem(final String item) {
+        mData.add(item);
+        // save separator position
+        mSeparatorsSet.add(mData.size() - 1);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mSeparatorsSet.contains(position) ? TYPE_SEPARATOR : TYPE_ITEM;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return TYPE_MAX_COUNT;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        int type = getItemViewType(position);
+        System.out.println("getView " + position + " " + convertView + " type = " + type);
+        if (convertView == null) {
+            holder = new ViewHolder();
+            switch (type) {
+                case TYPE_SEPARATOR:
+                    convertView = mInflater.inflate(R.layout.row_dia, null);
+                    holder.textView = (TextView)convertView.findViewById(R.id.text_dia);
+                    break;
+                case TYPE_ITEM:
+                    convertView = mInflater.inflate(R.layout.row, null);
+                    holder.textView = (TextView)convertView.findViewById(R.id.nombre_medicina);
+                    break;
+                default:
+                    convertView = mInflater.inflate(R.layout.row, null);
+                    holder.textView = (TextView)convertView.findViewById(R.id.nombre_medicina);
+                    break;
+            }
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder)convertView.getTag();
+        }
+        holder.textView.setText(mData.get(position));
+        return convertView;
+    }
+
+}
