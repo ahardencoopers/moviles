5d35d1af0c01ce5819c7ad9fb48f04e986578673
ahardencoopers <ahardencoopers@gmail.com>
2016-10-24 10:12:48 -0500

    Agregar layout y codigo para activity MedicamentoPendiente

--- PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/MedicamentoPendiente.java
+++ PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/MedicamentoPendiente.java
@@ -1,13 +1,42 @@
 package itesm.mx.proyecto_moviles;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MedicamentoPendiente extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MedicamentoPendiente extends ListActivity implements View.OnClickListener {
+
+    public ArrayList<MedicamentoPorTomar> getDataForListView() {
+        MedicamentoPorTomar medicamentoPorTomar;
+
+        ArrayList<MedicamentoPorTomar> listMedicamentosPorTomar = new ArrayList<MedicamentoPorTomar>();
+
+        medicamentoPorTomar = new MedicamentoPorTomar(R.drawable.logo, "Medicina", "1 Pastilla", "8:00", true);
+        listMedicamentosPorTomar.add(medicamentoPorTomar);
+        medicamentoPorTomar = new MedicamentoPorTomar(R.drawable.logo, "Medicina", "1 Pastilla", "16:00", false);
+        listMedicamentosPorTomar.add(medicamentoPorTomar);
+        medicamentoPorTomar = new MedicamentoPorTomar(R.drawable.logo, "Medicina", "1 Pastilla", "00:00", false);
+        listMedicamentosPorTomar.add(medicamentoPorTomar);
+
+        return listMedicamentosPorTomar;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicamento_pendiente);
+
+        ArrayList <MedicamentoPorTomar> arrayListMedicamentoPorTomar;
+        arrayListMedicamentoPorTomar = getDataForListView();
+
+        MedicamentoPorTomarAdapter adapterMedicamentosPorTomar = new MedicamentoPorTomarAdapter(this, arrayListMedicamentoPorTomar);
+        setListAdapter(adapterMedicamentosPorTomar);
+    }
+
+    public void onClick(View v) {
+        return;
     }
 }
---
+++ PROYECTO_MOVILES/app/src/main/java/itesm/mx/proyecto_moviles/MedicamentoPorTomarAdapter.java
@@ -0,0 +1,46 @@
+package itesm.mx.proyecto_moviles;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by achs on 24/10/16.
+ */
+public class MedicamentoPorTomarAdapter extends ArrayAdapter<MedicamentoPorTomar> {
+    public MedicamentoPorTomarAdapter(Context context, ArrayList<MedicamentoPorTomar> medicamentosPorTomar) {
+        super(context, 0, medicamentosPorTomar);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        MedicamentoPorTomar medicamentoPorTomar = getItem(position);
+
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_medicamento_pendiente, parent, false);
+
+            ImageView ivMedicamento = (ImageView) convertView.findViewById(R.id.image_medicamento);
+            TextView tvNombre = (TextView) convertView.findViewById(R.id.text_nombre_medicina);
+            TextView tvDosis = (TextView) convertView.findViewById(R.id.text_dosis);
+            TextView tvHorarioDosis = (TextView) convertView.findViewById(R.id.text_horario_dosis);
+            CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkBox_tomada);
+
+            ivMedicamento.setImageResource(medicamentoPorTomar.getIdImagen());
+            tvNombre.setText(medicamentoPorTomar.getNombre());
+            tvDosis.setText(medicamentoPorTomar.getDosis());
+            tvHorarioDosis.setText(medicamentoPorTomar.getHorario());
+        }
+
+        return convertView;
+    }
+}
