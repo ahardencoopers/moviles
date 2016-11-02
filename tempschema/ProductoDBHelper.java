3ef7adcb153ea57b099cd737c3442584d31afd3e
ahardencoopers <ahardencoopers@gmail.com>
2016-11-02 12:21:35 -0600

    Cambiar ProductoDBHelper.java para crear base de datos de RecuerdameMed

--- tempschema/ProductoDBHelper.java
+++ tempschema/ProductoDBHelper.java
@@ -68,57 +68,21 @@ public class ProductoDBHelper extends SQLiteOpenHelper {
                                 DOCS_DIR + " STRING, " + DOCS_CODIGOPOS + " STRING, " +
                                 DOCS_NUMERO + " STRING, " + DOCS_CORREO + " STRING)";
 
-            
-    /*private static final String CREATE_TABLE_EVENTO = "CREATE TABLE " +
-            TABLE_EVENTO + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, "  +
-            EVENTO_APARATO + " INTEGER, " + EVENTO_TIEMPOUSO + " REAL, " +
-            EVENTO_HORA_INICIO + " TEXT, " + EVENTO_FECHA + " TEXT)";*/
-
-    /*private static final String CREATE_TABLE_ELECTRODOMESTICO = "CREATE TABLE " +
-            TABLE_ELECTRODOMESTICO + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            ELECT_TIPOAPARATO + " INTEGER, " + ELECT_MARCA + " TEXT, " +
-            ELECT_FOTO + " BLOB)";
-
-    private static final String CREATE_TABLE_TIPO = "CREATE TABLE " +
-            TABLE_TIPO + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            TIPO_NOMBRE + " TEXT, " + TIPO_CONSUMO + " REAL)";
-
-    private static final String sTipos [][] = {{"Reproductor de DVDs","25"},
-            {"Batidora manual","140"},{"Televisión","150"},{"Computadora","150"},
-            {"Extractor de jugos","250"},{"Licuadora","350"},{"Lavadora","375"},
-            {"Bomba para agua","400"}, {"Refrigerador","575"}, {"Cafetera","700"},
-            {"Secadora de cabello","825"},{"Tostador eléctrico","900"},{"Plancha","1200"},
-            {"Aspiradora","1200"},{"Horno de microondas","1200"},{"Calefactor","1300"},
-            {"Aire acondicionado","2950"}};
-
-    private static void insertTipos(SQLiteDatabase db) {
-        for(int i = 0; i < 17; i++) {
-            try {
-                ContentValues values = new ContentValues();
-                values.put(TIPO_NOMBRE, sTipos[i][0]);
-                values.put(TIPO_CONSUMO, Integer.parseInt(sTipos[i][1]));
-                db.insert(TABLE_TIPO, null, values);
-            }catch(SQLException e){
-                Log.e("SQLADD", e.toString());
-            }
-        }
-    }*/
-
     public ProductoDBHelper(Context context) {
-
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE_TIPO);
-        Log.i("Producthelper onCreate", CREATE_TABLE_TIPO);
-        db.execSQL(CREATE_TABLE_ELECTRODOMESTICO);
-        Log.i("Producthelper onCreate", CREATE_TABLE_ELECTRODOMESTICO);
-        db.execSQL(CREATE_TABLE_EVENTO);
-        Log.i("Producthelper onCreate", CREATE_TABLE_EVENTO);
+        Log.i("Producthelper onCreate", CREATE_TABLE_MED);
+        db.execSQL(CREATE_TABLE_MED);
+
+        Log.i("Producthelper onCreate", CREATE_TABLE_USRS);
+        db.execSQL(CREATE_TABLE_USRS);
+
+        Log.i("Producthelper onCreate", CREATE_TABLE_DOCS);
+        db.execSQL(CREATE_TABLE_DOCS);
+
         insertTipos(db);
     }
 
@@ -130,9 +94,9 @@ public class ProductoDBHelper extends SQLiteOpenHelper {
                         " to " +
                         newVersion +
                         ", which will destroy all old data");
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EVENTO);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ELECTRODOMESTICO);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TIPO);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MED);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USRS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_DOCS);
         onCreate(db);
     }
 
